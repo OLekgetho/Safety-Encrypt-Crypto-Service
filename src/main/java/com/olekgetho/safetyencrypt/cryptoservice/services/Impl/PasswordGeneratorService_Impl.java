7558428869eb5765a92942344cabf77a30d42cf3
Ofentse Lekgetho <ofentse.lekgetho@acumensoft.net>
2026-02-08 18:58:36 +0200

    Updating the Password Generator to use SecureRandom

--- src/main/java/com/olekgetho/safetyencrypt/cryptoservice/services/Impl/PasswordGeneratorService_Impl.java
+++ src/main/java/com/olekgetho/safetyencrypt/cryptoservice/services/Impl/PasswordGeneratorService_Impl.java
@@ -5,12 +5,26 @@ import com.olekgetho.safetyencrypt.cryptoservice.services.PasswordGeneratorServi
 import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+
 @Service
 public class PasswordGeneratorService_Impl implements PasswordGeneratorService {
 
     @Override
     public String generatePassword(PasswordGenerator passwordGenerator) {
-        return RandomStringUtils.random(passwordGenerator.getLengthOfPassword()
-                ,passwordGenerator.isIncludeLetters(), passwordGenerator.isIncludeNumbers());
+        String pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom secureRandom = new SecureRandom();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (passwordGenerator.isIncludeSymbols()) {
+            pool += "!@#$%^&*";
+        }
+
+        for (int i = 0; i < passwordGenerator.getLengthOfPassword(); i++) {
+            int index = secureRandom.nextInt(pool.length());
+            stringBuilder.append(pool.charAt(index));
+        }
+
+        return stringBuilder.toString();
     }
 }
